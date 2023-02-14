448507272c4cf9f6cd016238d9ea2cb81d67c320
marcel-ml20aaf <125359855+marcel-ml20aaf@users.noreply.github.com>
2023-02-14 10:49:39 +0000

    starting LFS code.

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+if (false && false) {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+} else if (false && false) {
+	
+} else if (false && false) {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+} else {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+}
 basic.forever(function () {
 	
 })
